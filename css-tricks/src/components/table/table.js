e3e0820f37ec13602c822d861d94d5ee603dfebe
Okan <okanakinci000@gmail.com>
2022-07-04 18:04:38 +0300

    tables

--- css-tricks/src/components/table/table.js
+++ css-tricks/src/components/table/table.js
@@ -6,9 +6,53 @@ function Table() {
         console.log(people);
     });
     return (
-        <div className="table">
-            <div class="gradient-border">
+        <div>
+            <div className="tablesec">
                 <table>
+                    <tr>
+                        <td>Aaa</td>
+                        <td rowSpan="2">Bbb</td>
+                        <td>Ccc</td>
+                        <td>Ddd</td>
+                        <td>Eee</td>
+                        <td colSpan="3">Fff</td>
+                    </tr>
+                    <tr>
+                        <td>Aaa</td>
+                        <td>Bbb</td>
+                        <td>Ccc</td>
+                        <td>Ddd</td>
+                        <td>Eee</td>
+                        <td>Fff</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2">Aaa</td>
+                        <td>Bbb</td>
+                        <td>Ccc</td>
+                        <td>Ddd</td>
+                        <td rowSpan="3">Eee</td>
+                        <td>Fff</td>
+                    </tr>
+                    <tr>
+                        <td>Aaa</td>
+                        <td>Bbb</td>
+                        <td>Ccc</td>
+                        <td>Ddd</td>
+                        <td>Eee</td>
+                        <td>Fff</td>
+                    </tr>
+                    <tr>
+                        <td>Aaa</td>
+                        <td>Bbb</td>
+                        <td>Ccc</td>
+                        <td>Ddd</td>
+                        <td>Eee</td>
+                        <td>Fff</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="table">
+                <table class="gradient-border">
                     <thead>
                         <tr>
                             <th> Name </th>
